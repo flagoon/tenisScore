4a0a4b0618ffa373dd0380cb4feabe8d592b8459
Pawel Kochanek <pawel.kochanek@gmail.com>
2018-10-10 19:46:27 +0200

    Refactor

--- src/tennisScore.js
+++ src/tennisScore.js
@@ -1,4 +1,4 @@
-const decideAdvantage = (scoreOne, scoreTwo) => {
+const decideWinningPlayer = (scoreOne, scoreTwo) => {
     const diff = scoreOne - scoreTwo;
     if (diff > 0) {
         return 1;
@@ -10,35 +10,18 @@ const decideAdvantage = (scoreOne, scoreTwo) => {
 };
 
 const tennisScore = (playerOneScore, playerTwoScore) => {
-    const scoreNames = {
-        "0": "Love",
-        "1": "Fifteen",
-        "2": "Thirty",
-        "3": "Forty"
-    };
+    const scoreNames = ["Love", "Fifteen", "Thirty", "Forty"];
 
-    const absoluteDifference = Math.abs(playerOneScore - playerTwoScore);
+    const winner = decideWinningPlayer(playerOneScore, playerTwoScore);
 
-    const advantage = decideAdvantage(playerOneScore, playerTwoScore);
-
-    if (advantage === 0) {
+    if (winner === 0) {
         return playerOneScore > 3
             ? "Deuce"
             : `${scoreNames[playerOneScore]}-All`;
     } else if (playerOneScore >= 4 || playerTwoScore >= 4) {
-        if (absoluteDifference >= 2) {
-            if (playerOneScore > playerTwoScore) {
-                return "Win for player1";
-            } else {
-                return "Win for player2";
-            }
-        } else if (absoluteDifference === 1) {
-            if (playerOneScore > playerTwoScore) {
-                return "Advantage player1";
-            } else {
-                return "Advantage player2";
-            }
-        }
+        return Math.abs(playerOneScore - playerTwoScore) >= 2
+            ? `Win for player${winner}`
+            : `Advantage player${winner}`;
     } else {
         return `${scoreNames[playerOneScore]}-${scoreNames[playerTwoScore]}`;
     }
