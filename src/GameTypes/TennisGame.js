9e7812d880a7803e2f41737c7f01074f3393bd05
Paweł Kochanek <pawel.kochanek@gmail.com>
2018-10-11 08:44:34 +0200

    Added new game mode

--- src/GameTypes/TennisGame.js
+++ src/GameTypes/TennisGame.js
@@ -1,4 +1,15 @@
 class TennisGame {
+    constructor() {
+        this._scoreName = ['Love', 'Fifteen', 'Thirty', 'Forty'];
+        this._winningBreakPoint = 4;
+        this._pointsAdvantage = 2;
+        this._winnerName = 'player';
+    }
+
+    get winnerName() {
+        return this._winnerName;
+    }
+
     get winningBreakPoint() {
         return this._winningBreakPoint;
     }
@@ -7,14 +18,30 @@ class TennisGame {
         return this._pointsAdvantage;
     }
 
-    constructor() {
-        this._scoreName = ['Love', 'Fifteen', 'Thirty', 'Forty'];
-        this._winningBreakPoint = 4;
-        this._pointsAdvantage = 2;
-    }
-
     get scoreName() {
         return this._scoreName;
+    }
+
+    winningMessage(player) {
+        return `Win for ${player.winningPlayer}`;
+    }
+
+    advantageMessage(player) {
+        return `Advantage ${player.winningPlayer}`;
+    }
+
+    tieResult(playerScore, scoreNames) {
+        return playerScore > 3 ? 'Deuce' : `${scoreNames[playerScore]}-All`;
+    }
+
+    handleAdvantage(player) {
+        if (player.score[0] >= this._winningBreakPoint
+            || player.score[1] >= this._winningBreakPoint) {
+            return Math.abs(player.score[0] - player.score[1]) >= this._pointsAdvantage
+                ? this.winningMessage(player)
+                : this.advantageMessage(player);
+        }
+        return `${this._scoreName[player.score[0]]}-${this._scoreName[player.score[1]]}`;
     }
 
 }
--- src/MatchResult.js
+++ src/MatchResult.js
@@ -8,37 +8,19 @@ class MatchResult {
     decideWinningPlayer() {
         const scoreDifference = this.scoreOne - this.scoreTwo;
         if (scoreDifference > 0) {
-            return {winningPlayer: 'player1'};
-        } else if (scoreDifference < 0)
-        {
-            return {winningPlayer: 'player2'};
+            return { winningPlayer: `${this.gameType.winnerName}1`, score: [this.scoreOne, this.scoreTwo] };
+        } else if (scoreDifference < 0) {
+            return { winningPlayer: `${this.gameType.winnerName}2`, score: [this.scoreOne, this.scoreTwo] };
         }
         return 'none';
-    }
-
-    static winningMessage(player) {
-        return `Win for ${player.winningPlayer}`;
-    }
-
-    static advantageMessage(player) {
-        return `Advantage ${player.winningPlayer}`;
-    }
-
-    static decideTieType(playerScore, scoreNames) {
-        return playerScore > 3 ? 'Deuce' : `${scoreNames[playerScore]}-All`;
     }
 
     convertScore() {
         const playerInAdvance = this.decideWinningPlayer();
         if (playerInAdvance === 'none') {
-            return MatchResult.decideTieType(this.scoreOne, this.gameType.scoreName);
-        } else if (this.scoreOne >= this.gameType.winningBreakPoint
-            || this.scoreTwo >= this.gameType.winningBreakPoint) {
-            return Math.abs(this.scoreOne - this.scoreTwo) >= this.gameType.pointsAdvantage
-                ? MatchResult.winningMessage(playerInAdvance)
-                : MatchResult.advantageMessage(playerInAdvance);
+            return this.gameType.tieResult(this.scoreOne, this.gameType.scoreName);
         }
-        return `${this.gameType.scoreName[this.scoreOne]}-${this.gameType.scoreName[this.scoreTwo]}`;
+        return this.gameType.handleAdvantage(playerInAdvance);
     }
 }
 
